314dc5eb1ec1e1f92e27366f17ead3310e8c4511
Shane <shanesirohi1@gmail.com>
2025-02-17 19:39:08 +0530

    new

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://radianttextingbackend.onrender.com/api" : "https://radianttextingbackend.onrender.com/api",
+  baseURL: "https://radianttextingbackend.onrender.com/api",
   withCredentials: true,
 });
