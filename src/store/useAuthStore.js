24b3dea5db6e6972cf6fd73d0f13c77c96c28ea5
Shane <shanesirohi1@gmail.com>
2025-02-17 18:52:09 +0530

    fixed some shit errors

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "https://radianttextingbackend.onrender.com/" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "https://radianttextingbackend.onrender.com" : "https://radianttextingbackend.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
